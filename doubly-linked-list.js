08926eed43a84c70d9f1e1753c5f4e0f474546fd
Lloyd Akresi <lloydakresi03@gmail.com>
2023-01-01 22:31:12 +0000

    completed

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -9,14 +9,41 @@ class DoublyLinkedListNode {
 class DoublyLinkedList {
   constructor() {
     // Your code here
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
   addToHead(val) {
     // Your code here
+    const node = new DoublyLinkedListNode(val);
+    if (!this.head){
+      this.head = node; // node is both the first and last in the list
+      this.tail = node;
+    }else{
+      node.next = this.head; // node will point to the current head since it will replace it as the new head pointer
+      this.head.prev = node; // node will become #this.head's preceding node since node will be pointing to it
+      this.head = node; // node will now become the new head pointer
+
+    }
+    this.length++;
   }
 
   addToTail(val) {
     // Your code here
+    const node = new DoublyLinkedListNode(val);
+
+    if(!this.head){
+      this.head = node;
+      this.tail = node;
+    }else{
+      node.prev = this.tail;
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+
+
   }
 
   // You can use this function to help debug
--- linked-list.js
+++ linked-list.js
@@ -8,14 +8,38 @@ class LinkedListNode {
 class LinkedList {
   constructor() {
     // Your code here
+    this.head = null;
+    this.length = 0;
   }
 
   addToHead(val) {
     // Your code here
+    const node = new LinkedListNode(val);
+    if (!this.head){this.head = node
+    }else{
+      node.next = this.head;
+      this.head = node;
+
+    }
+    this.length++;
   }
 
   addToTail(val) {
     // Your code here
+    const node = new LinkedListNode(val);
+    let current = this.head; // start at the beginning
+
+    if(current === null){
+      this.head = node;
+      this.length++;
+    }else{
+      while(current.next !== null){
+        current = current.next;
+      }
+
+      current.next = node;
+      this.length++;
+    }
   }
 
   // You can use this function to help debug
